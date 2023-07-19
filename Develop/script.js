9b7becff55cb98f8e52ce38acb4d3863537441ef
Ha Young Jang <pauljjang77@gmail.com>
2023-07-18 22:14:12 -0400

    updated global variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment Code - what is the point of the generateBtn 
 var generateBtn = document.querySelector("#generate");
-
+var includeUppercase = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,25 +9,26 @@ function writePassword() {
   passwordText.value = password;
 
 }
-//
-function generatePassword() {
+
+function generatePassword(passwordLength,) {
   var userchoice = userInfo(); 
-  if 
+  
 }
 function userInfo() {
   var passwordLength = parseInt(prompt("Please enter the length of your password. Choose between 8 and 128 characters."));
   console.log(passwordLength)
   if (passwordLength < 8 || passwordLength >= 128) {
-    alert("Please include at least 8 characters.")
+    alert("How many characters are in your password? Please choose between 8 - 128")
     return null; 
   }
   if (Number.isNaN(passwordLength)){
-    alert("Please include at least 8 characters.")
+    alert("How many characters are in your password? Please choose between 8 - 128")
     return null;
   }
   var includeUppercase = confirm("Would you like uppercase characters?");
-  var includeNumeric = ("Would you like numeric characters?");
-  var includeSpecial = ("Would you like special characters?");
+  var includeLowercase = confirm("Would you like uppercase characters")
+  var includeNumeric = confirm("Would you like numeric characters?");
+  var includeSpecial = confirm("Would you like special characters?");
 
 
 var finalpassword = {
